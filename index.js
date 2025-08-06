0617ce80a7b47f4719b03d56375c4739147e6717
rimardev <catamarce.bs@gmail.com>
2025-08-05 21:35:17 -0400

    Se agregan Numeros y validacion de cero

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-let data = "";
+let dataDisplay = "";
 let numero1 = 0;
 let numero2 = 0;
 let operador = "";
 
 const display = document.getElementById('display');
 
-function showVisor(addnum) {
-  data += addnum;
-  console.log(data);
-  display.innerText = data;  
+function muestraDisplay(addnum) {
+  dataDisplay += addnum;
+  console.log(dataDisplay);
+  display.innerText = dataDisplay;  
 }
 
 function saveNumber1(numero) {
   numero1 = numero;
-  data = "";
+  dataDisplay = "";
   display.innerText = "0";  
 }
 
 function saveNumber2(numero) {
   numero2 = numero;
-  data = "";
+  dataDisplay = "";
   display.innerText = "0";  
 }
 
@@ -43,19 +43,12 @@ function resolver(numero1, numero2, operador) {
   };  
 };
 
-const num1 = document.getElementById("1");
-num1.addEventListener("click",() => {
-  showVisor(num1.textContent);
-});
 
-const num2 = document.getElementById("2");
-num2.addEventListener("click",() => {
-  showVisor(num2.textContent);
-});
 
+// OPERADORES *******************************
 const borrar = document.getElementById("c");
 borrar.addEventListener("click",() => {
-  data = "";
+  dataDisplay = "";
   numero1 = 0;
   numero2 = 0;
   display.innerText = "0";
@@ -66,5 +59,55 @@ equal.addEventListener("click",() => {
   resolver(numero1, numero2, operador);
 });
 
+// NUMEROS **********************************
+const num1 = document.getElementById("1");
+num1.addEventListener("click",() => {
+  muestraDisplay(num1.textContent);
+});
 
+const num2 = document.getElementById("2");
+num2.addEventListener("click",() => {
+  muestraDisplay(num2.textContent);
+});
 
+const num3 = document.getElementById("3");
+num3.addEventListener("click",() => {
+  muestraDisplay(num3.textContent);
+});
+
+const num4 = document.getElementById("4");
+num4.addEventListener("click",() => {
+  muestraDisplay(num4.textContent);
+});
+
+const num5 = document.getElementById("5");
+num5.addEventListener("click",() => {
+  muestraDisplay(num5.textContent);
+});
+
+const num6 = document.getElementById("6");
+num6.addEventListener("click",() => {
+  muestraDisplay(num6.textContent);
+});
+
+const num7 = document.getElementById("7");
+num7.addEventListener("click",() => {
+  muestraDisplay(num7.textContent);
+});
+
+const num8 = document.getElementById("8");
+num8.addEventListener("click",() => {
+  muestraDisplay(num8.textContent);
+});
+
+const num9 = document.getElementById("9");
+num9.addEventListener("click",() => {
+  muestraDisplay(num9.textContent);
+});
+
+const num0 = document.getElementById("0");
+num0.addEventListener("click",() => {
+  if(dataDisplay != ""){
+    muestraDisplay(num0.textContent);
+  };  
+});
